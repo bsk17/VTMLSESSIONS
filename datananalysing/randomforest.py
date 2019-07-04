8774f98b317a0be204a1a41d1169d25079f843f6
bsk17 <sk.bijin@gmail.com>
2019-07-04 16:27:46 +0530

    4 july commit 3

--- datananalysing/randomforest.py
+++ datananalysing/randomforest.py
@@ -15,7 +15,7 @@ dataframe = read_csv(filename)
 X = dataframe.iloc[:, [2, 3]].values
 Y = dataframe.iloc[:, 4].values
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=7)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
 
 
 sc = StandardScaler()
